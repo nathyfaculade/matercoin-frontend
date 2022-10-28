3b8ee230ec5d0da88ee837b7ad9f07378a3afde4
Nathy <91018534+nathyfaculade@users.noreply.github.com>
2022-10-28 19:28:59 -0300

    AKMFAMKF

--- src/app/pages/base-cadastro/base-cadastro.module.ts
+++ src/app/pages/base-cadastro/base-cadastro.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseCadastroComponent } from './base-cadastro.component';
 import { FormsModule } from '@angular/forms';
+import { DividerModule } from 'primeng/divider';
 
 
 
@@ -9,7 +10,8 @@ import { FormsModule } from '@angular/forms';
   declarations: [BaseCadastroComponent],
   imports: [
     FormsModule,
-    CommonModule
+    CommonModule,
+    DividerModule
   ], exports:[BaseCadastroComponent]
 })
 export class BaseCadastroModule { }
