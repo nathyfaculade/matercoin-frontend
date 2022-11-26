8616319660d4ddd13e67ffcf980424ed50fadb44
Samuel Victor Alves Antunes <samuewicto.alves@gmail.com>
2022-11-26 18:26:48 -0300

    Integração com Moodle (front)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,31 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
 import { AbstractService } from './service/abstract.service';
+import { UsuarioService } from './service/usuario.service';
 
 export const spinner = (value) => {
     showSpinner = value;
-}
+};
 
 export var showSpinner = false;
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
 })
 export class AppComponent {
-
-    constructor(private primengConfig: PrimeNGConfig) {
+    constructor(
+        private primengConfig: PrimeNGConfig,
+        private route: ActivatedRoute,
+        private service: UsuarioService
+    ) {
+        this.route.queryParams.subscribe((p) => {
+            if (p['token']) {
+                localStorage.setItem('auth', p['token']);
+                console.log(localStorage.getItem('auth'));
+            }
+        });
         //AbstractService.setToken("abc123")
     }
 
@@ -22,7 +33,24 @@ export class AppComponent {
         return showSpinner;
     }
 
+    async getToken() {
+        const p = await this.route.queryParams;
+        console.log(p);
+        if (p['token']) {
+            localStorage.setItem('auth', p['token']);
+            console.log(localStorage.getItem('auth'));
+        }
+        return localStorage.getItem('auth');
+    }
+
     ngOnInit() {
+        this.getToken().then((t) => {
+            this.service.validateToken(t).subscribe((v) => {
+                localStorage.setItem('id-usuario', v.userId);
+                localStorage.setItem('username', v.username);
+            });
+        });
+
         this.primengConfig.ripple = true;
     }
 }
--- src/app/service/usuario.service.ts
+++ src/app/service/usuario.service.ts
@@ -1,14 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Usuario } from '../model/usuario';
 import { AbstractService } from './abstract.service';
+import { HttpRequest, UriBuilder } from './http-requests';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
-export class UsuarioService extends AbstractService<Usuario>{
-
+export class UsuarioService extends AbstractService<Usuario> {
     endpoint: string = 'usuarios';
+
+    validateToken(token = ''): Observable<any> {
+        return new HttpRequest<any>(this.http)
+            .endpoint(`localiza`)
+            .uri(new UriBuilder().param('token', token).build())
+
+            .doGet();
+    }
 
     constructor(http: HttpClient) {
         super(http);
