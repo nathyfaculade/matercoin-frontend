dd183a8ab8b8a40f78ce01d8a8b5be296f143b06
Nathy <91018534+nathyfaculade@users.noreply.github.com>
2022-12-09 19:36:14 -0300

    aula 9/12

--- src/app/pages/usuario/criterios/criterios.component.ts
+++ src/app/pages/usuario/criterios/criterios.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {AutoFocusModule} from 'primeng/autofocus';
+import {ButtonModule} from 'primeng/button';
+import { spinner } from 'src/app/app.component';
 
 @Component({
   selector: 'app-criterios',
   templateUrl: './criterios.component.html',
-  styleUrls: ['./criterios.component.scss']
+  styleUrls: ['./criterios.component.scss'],
+
 })
 export class CriteriosComponent implements OnInit {
+  service: any;
+    msg: any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
